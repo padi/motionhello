069ab0345ebfea59d47f265b49dff0da657bcfd8
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-09-03 12:20:59 +0800

    Hello world alert done and updated gitignore

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
-class AppDelegate
+class AppDelegate # responsible for controlling your application, is it like a 'controller'?
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hello World! This is my first app in RubyMotion! Woohoo!"
+    alert.show
     true
   end
 end
